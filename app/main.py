8ff3c662c92ac50e327f410f31d131f98638639d
aribekkuu <alibekzhazit05@gmail.com>
2025-11-28 19:47:03 +0500

    fix tests

--- app/main.py
+++ app/main.py
@@ -1,19 +1,19 @@
 from fastapi import FastAPI
-
+from contextlib import asynccontextmanager
 from app.core.db import AsyncSessionMaker, init_db
 from app.routers.routes import router as api_router
 from app.services.seed import seed_data
 
-
-app = FastAPI(title="Pizza Task API (Clean Architecture)", version="2.0.0")
-
-
-@app.on_event("startup")
-async def on_startup() -> None:
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # startup
     await init_db()
     async with AsyncSessionMaker() as session:
         await seed_data(session)
+    yield
 
+
+app = FastAPI(title="Pizza Task API (Clean Architecture)", version="2.0.0", lifespan=lifespan)
 
 app.include_router(api_router)
 
