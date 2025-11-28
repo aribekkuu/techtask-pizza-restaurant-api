e794be8f1e32986d762911eac2501f98caa4bdaf
aribekkuu <alibekzhazit05@gmail.com>
2025-11-28 19:12:37 +0500

    fixing

--- src/core/db.py
+++ src/core/db.py
@@ -2,11 +2,8 @@ from __future__ import annotations
 
 from typing import AsyncGenerator
 
-from sqlalchemy.ext.asyncio import (
-    AsyncSession,
-    async_sessionmaker,
-    create_async_engine,
-)
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine,
+
 from sqlalchemy.orm import DeclarativeBase
 
 
